d118ab715420dd2395202ff5460680079492d9ae
ThilinaJayamal <thilinajayamal271@gmail.com>
2024-09-19 20:55:33 +0530

    selection sort

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
- const numbers = [5,8,2,17,1,3,9];
-  return (
-    <di>
-      {
-        numbers.map((num,index)=>{
-          return(
-            <h1 key={index} className='text-red-600'>{num}</h1>
-          )
-        })
+  const [numbers, setNumbers] = useState([5, 8, 2, 17, 1, 3, 9]);
+
+  const selectionSort = async() => {
+    for (let i = 0; i < numbers.length; i++) {
+      for (let j = i + 1; j < numbers.length; j++) {
+        if (numbers[i] > numbers[j]) {
+          let temp = numbers[i];
+          numbers[i] = numbers[j];
+          numbers[j] = temp;
+          setNumbers([...numbers]);
+        }
       }
-    </di>
+    }
+    console.log(...numbers);
+  }
+
+  return (
+    <>
+      <di className='flex-1 flex-row'>
+        {
+          numbers.toString()
+        }
+      </di>
+      <h1 onClick={() => selectionSort()}>sort</h1>
+    </>
   )
 }
 
